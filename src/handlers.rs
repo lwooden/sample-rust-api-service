e8da5ad91aca7f0260adff5e47f194bbb065ff25
lwooden <lowell.wooden@gmail.com>
2022-08-06 23:20:05 -0400

    updates

--- src/handlers.rs
+++ src/handlers.rs
@@ -1,7 +1,7 @@
 use super::models::*;
 use super::state::AppState;
 use actix_web::{web, HttpRequest, HttpResponse};
-use std::{env};
+use std::env;
 
 // Health Check Handler
 pub async fn health_check(app_state: web::Data<AppState>) -> HttpResponse {
@@ -32,18 +32,53 @@ pub async fn check_environment() -> HttpResponse {
 
     if valid_env == false {
         HttpResponse::Ok().json(format!(
-            "It look like you are running in a {} environment!",
-            env
+            "It looks like you are running in a {} environment!",env
         ))
     } else {
         HttpResponse::NotFound().json("Environment not set!")
     }
 }
 
-
 pub async fn get_sum(payload: web::Json<Sum>) -> HttpResponse {
     let sum: i32 = payload.val_1 + payload.val_2;
     HttpResponse::Ok().json(sum)
+}
+
+pub async fn get_podInfo() -> HttpResponse {
+    let node = match env::var_os("NODE_NAME") {
+        Some(n) => n.into_string().unwrap(),
+        None => panic!("You are not in a Kubernetes Environment!"),
+    };
+
+    let pod = match env::var_os("POD_NAME") {
+        Some(p) => p.into_string().unwrap(),
+        None => panic!("You are not in a Kubernetes Environment!"),
+    };
+
+    let pod_ip = match env::var_os("POD_IP") {
+        Some(pi) => pi.into_string().unwrap(),
+        None => panic!("You are not in a Kubernetes Environment!"),
+    };
+
+    let pod_namespace = match env::var_os("POD_NAMESPACE") {
+        Some(pn) => pn.into_string().unwrap(),
+        None => panic!("You are not in a Kubernetes Environment!"),
+    };
+
+    let pod_service = match env::var_os("POD_SERVICE") {
+        Some(ps) => ps.into_string().unwrap(),
+        None => panic!("You are not in a Kubernetes Environment!"),
+    };
+
+    let pod_metadata = PodMetadata {
+        node: node,
+        pod_name: pod,
+        pod_ip: pod_ip,
+        pod_namespace: pod_namespace,
+        pod_service_account: pod_service,
+    };
+
+    HttpResponse::Ok().json(pod_metadata)
 }
 
 pub async fn get_pokemon(req: HttpRequest) -> HttpResponse {
@@ -51,7 +86,7 @@ pub async fn get_pokemon(req: HttpRequest) -> HttpResponse {
     let id: String = req.match_info().get("id").unwrap().parse().unwrap();
 
     // Set the url
-    let url = format!("https://pokeapi.co/api/v2/pokemon/{}", id); 
+    let url = format!("https://pokeapi.co/api/v2/pokemon/{}", id);
     // println!("{}",url);
 
     // Create a client and send the request
@@ -66,32 +101,13 @@ pub async fn get_pokemon(req: HttpRequest) -> HttpResponse {
     match response.status() {
         reqwest::StatusCode::OK => match response.json::<Pokemon>().await {
             Ok(parsed) => HttpResponse::Ok().json(parsed),
-            Err(_) => HttpResponse::BadRequest().json("Something went wrong!")
+            Err(_) => HttpResponse::BadRequest().json("Something went wrong!"),
         },
         other => {
             panic!("Uh oh something went really wrong! {:?}", other)
         }
     }
 
-    // HttpResponse::Ok().json("some")
-
 }
 
 
-// pub async fn fetch_cat_facts() -> HttpResponse {
-//     let url = "https://cat-fact.herokuapp.com/facts/random";
-
-//     let client = reqwest::Client::new();
-
-//     let response = client.get(url).send().await.unwrap();
-
-//     match response.status() {
-//         reqwest::StatusCode::OK => match response.json::<CatFact>().await {
-//             Ok(parsed) => HttpResponse::Ok().json(parsed),
-//             Err(_) => HttpResponse::BadRequest().json("Something went wrong!"),
-//         },
-//         other => {
-//             panic!("Uh oh! Something unexpected happened: {:?}", other);
-//         }
-//     }
-// }
